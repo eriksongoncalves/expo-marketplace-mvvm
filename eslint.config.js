a292d38758606d5b801c791a25c5f13d87cf0593
Erikson Gonçalves Bezerra <eriksongoncalves@yahoo.com.br>
2025-11-16 17:43:57 -0300

    feat: initial config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,82 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require('eslint/config')
+const tseslint = require('@typescript-eslint/eslint-plugin')
+const tsParser = require('@typescript-eslint/parser')
+const reactPlugin = require('eslint-plugin-react')
+const reactHooksPlugin = require('eslint-plugin-react-hooks')
+const importPlugin = require('eslint-plugin-import')
+const prettierPlugin = require('eslint-plugin-prettier')
+const prettierConfig = require('eslint-config-prettier')
 
 module.exports = defineConfig([
-  expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ['node_modules', 'android', 'ios', '.expo', 'dist/*']
   },
-]);
+
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true }
+      }
+    },
+
+    settings: {
+      react: { version: 'detect' },
+      import: {
+        resolver: {
+          typescript: {}
+        }
+      }
+    },
+
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      '@typescript-eslint': tseslint,
+      import: importPlugin,
+      prettier: prettierPlugin
+    },
+
+    rules: {
+      ...prettierConfig.rules,
+
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 100,
+          tabWidth: 2,
+          useTabs: false,
+          trailingComma: 'none',
+          arrowParens: 'avoid',
+          semi: false,
+          bracketSpacing: true,
+          singleQuote: true
+        }
+      ],
+
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/display-name': 'off',
+
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        }
+      ],
+
+      'no-console': 'warn'
+    }
+  }
+])
