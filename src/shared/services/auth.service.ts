19439260a3814b7484031cb7c92979a2cbc0c062
Erikson Gonçalves Bezerra <eriksongoncalves@yahoo.com.br>
2025-11-17 12:49:58 -0300

    feat: auth service

---
+++ src/shared/services/auth.service.ts
@@ -0,0 +1,8 @@
+import { RegisterHttpParams, RegisterHttpResponse } from '../interfaces/http/register'
+import { marketplaceApiClient } from '../api/marketplace.api'
+
+export const register = async (userData: RegisterHttpParams) => {
+  const { data } = await marketplaceApiClient.post<RegisterHttpResponse>('/auth/register', userData)
+
+  return data
+}
