fc2513d8ed69bcde443ef0a682fb57ff1d051880
Erikson Gonçalves Bezerra <eriksongoncalves@yahoo.com.br>
2025-11-18 11:35:16 -0300

    feat: register fields

--- src/view-models/register/register.view.tsx
+++ src/view-models/register/register.view.tsx
@@ -1,22 +1,52 @@
-import { View } from 'react-native'
+import { ScrollView, Text, TouchableOpacity } from 'react-native'
+import { router } from 'expo-router'
 
 import { AppInputController } from '@/shared/components/AppInputController'
+import { AuthFormHeader } from '@/shared/components/AuthFormHeader'
+import { KeyboardContainer } from '@/shared/components/KeyboardContainer'
 import { useRegisterViewModel } from './useRegister.viewModel'
 
 type RegisterViewProps = ReturnType<typeof useRegisterViewModel>
 
-export default function RegisterView({ control }: RegisterViewProps) {
+export default function RegisterView({ onSubmit, control }: RegisterViewProps) {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <AppInputController control={control} name="email" leftIcon="mail-outline" label="E-mail" />
+    <KeyboardContainer>
+      <ScrollView className="flex-1 px-[40px]">
+        <AuthFormHeader title="Crie sua conta" subTitle="Informe seus dados pessoais e de acesso" />
+        <AppInputController leftIcon="person-outline" label="NOME" control={control} name="name" />
 
-      <AppInputController
-        control={control}
-        name="password"
-        label="Senha"
-        leftIcon="lock-closed-outline"
-        secureTextEntry
-      />
-    </View>
+        <AppInputController
+          leftIcon="call-outline"
+          label="TELEFONE"
+          control={control}
+          name="phone"
+        />
+        <AppInputController leftIcon="mail-outline" label="E-MAIL" control={control} name="email" />
+
+        <AppInputController
+          leftIcon="lock-closed-outline"
+          label="SENHA"
+          control={control}
+          name="password"
+          secureTextEntry
+        />
+
+        <AppInputController
+          leftIcon="lock-closed-outline"
+          label="CONFIRMAR SENHA"
+          control={control}
+          name="confirmPassword"
+          secureTextEntry
+        />
+
+        <TouchableOpacity onPress={onSubmit}>
+          <Text>Registrar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => router.push('/login')}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </KeyboardContainer>
   )
 }
